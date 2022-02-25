df9ef2bb0e6777427f90e173c82f71967dd0853a
Hubert Ryan <hubertryanofficial@gmail.com>
2022-02-25 04:58:43 -0300

    added to lib paste

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,3 @@
-function useLastCall(
-  ms: number,
-  lastCallTime: number,
-  functionToCall: () => void
-): any {
-  if (!ms || !lastCallTime || !functionToCall)
-    throw "All proprety are required to useLastCall hook work";
-
-  const NOW_DATE = new Date();
-  const LAST_TIME_DATE = new Date(lastCallTime);
-
-  const getSecondsDifference =
-    (LAST_TIME_DATE.getTime() - NOW_DATE.getTime()) / 1000;
-  const msToSeconds = ms * 1000;
-
-  if (getSecondsDifference >= msToSeconds) {
-    return functionToCall();
-  }
-}
+import { useLastCall } from "./lib";
 
 export default useLastCall;
---
+++ src/lib/index.ts
@@ -0,0 +1,21 @@
+function useLastCall(
+  ms: number,
+  lastCallTime: number,
+  functionToCall: () => void
+): any {
+  if (!ms || !lastCallTime || !functionToCall)
+    throw "All proprety are required to useLastCall hook work";
+
+  const NOW_DATE = new Date();
+  const LAST_TIME_DATE = new Date(lastCallTime);
+
+  const getSecondsDifference =
+    (LAST_TIME_DATE.getTime() - NOW_DATE.getTime()) / 1000;
+  const msToSeconds = ms * 1000;
+
+  if (getSecondsDifference >= msToSeconds) {
+    return functionToCall();
+  }
+}
+
+export { useLastCall };
